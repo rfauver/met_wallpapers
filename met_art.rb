5cff4beab30163159ac6e40127a8d121ec5200a2
Ryan Fauver <ryan.fauver@gmail.com>
2020-04-02 18:16:01 -0700

    replace old scraping with Nokogiri + various refresh updates

--- met_art.rb
+++ met_art.rb
@@ -1,9 +1,16 @@
 class MetArt
+  require 'fileutils'
   require 'csv'
-  require 'net/http'
+  require 'nokogiri'
   require 'fastimage'
   require 'open-uri'
-  require 'RMagick'
+  require 'rmagick'
+
+  FOLDER_NAME = 'wallpapers'.freeze
+
+  def initialize
+    FileUtils.mkdir_p(File.join(File.dirname(__FILE__), FOLDER_NAME))
+  end
 
   def fetch(total: 10, department: nil)
     artworks = if department.nil?
@@ -25,16 +32,16 @@ class MetArt
   end
 
   def download_image(artwork)
-    response = Net::HTTP.get(URI(artwork["Link Resource"]))
-    match_data = response.match(/selectedOrDefaultDownload.*(http.*\.jpg)/)
-    print_error('Failed') and return if match_data.nil?
-    image_link = match_data.captures.first
-    width, height = FastImage.size(image_link)
-    print_error('Could not get size') and return if [width, height].any?(&:nil?)
+    page = Nokogiri::HTML(open(artwork["Link Resource"]))
+    image_url = page&.css('.gtm__download__image')&.first
+                  &.attributes&.fetch('href', nil)&.value
+    print_error('Failed to find image URL') and return if image_url.nil?
+    width, height = FastImage.size(image_url)
+    print_error('Could not get image') and return if [width, height].any?(&:nil?)
     print_error('Too Small') and return if width < 2560 && height < 1440
     print_error('Not landscape') and return if height > width
     filename = "#{artwork['Title'].downcase.gsub(' ', '_')}.jpg"
-    open(image_link) do |image|
+    open(image_url) do |image|
       File.open(filename, 'wb') do |file|
         file.puts image.read
       end
@@ -48,10 +55,10 @@ class MetArt
     image.change_geometry!('2560x1440') do |cols, rows, img|
      img.resize!(cols, rows)
     end
-    title = artwork['Title'].strip
-    artist = artwork['Artist Display Name'].strip
-    year = artwork['Object End Date'].strip
-    caption = "#{title}\n#{artist}\n#{year}"
+    title = artwork['Title']&.strip
+    artist = artwork['Artist Display Name']&.strip
+    year = artwork['Object End Date']&.strip
+    caption = [title, artist, year].compact.join("\n")
     Magick::Draw.new.annotate(image, 0, 0, 20, 15, caption) do
       self.fill = 'white'
       self.font = 'Palatino-Italic'
@@ -59,26 +66,38 @@ class MetArt
       self.pointsize = 30
     end
     image = background.composite(image, Magick::CenterGravity, Magick::OverCompositeOp)
-    image.write("#{path.gsub('.jpg', '')}_wallpaper.jpg")
+    image.write(output_path(path))
   end
 
   # MetObjects.csv can be downloaded here https://github.com/metmuseum/openaccess
   def data
-    @data ||= read_csv('MetObjects.csv').select do |h|
-      h["Is Public Domain"] == "True"
-    end
+    @data ||= read_csv('MetObjects.csv')
   end
 
   private
 
   def read_csv(path)
-    csv_data = CSV.open(path).read
-    keys = csv_data.shift
-    csv_data.map { |items| items.map.with_index { |item, i| [keys[i], item] }.to_h }
+    csv_data = []
+    CSV.foreach(path, headers: true) do |row|
+      csv_data << row.to_h if row["Is Public Domain"] == "True"
+    end
+    csv_data
   end
 
   def print_error(message)
     puts message
     true
   end
+
+  def output_path(path)
+    output = File.join(File.dirname(__FILE__), FOLDER_NAME, path)
+    while File.exist?(output)
+      match_data = output.match(/(\d+)\.jpg$/)
+      file_number = match_data.nil? ? 0 : match_data.captures.first.to_i + 1
+      output.gsub!(/_?\d*\.jpg$/, "_#{file_number}.jpg")
+    end
+    output
+  end
 end
+
+MetArt.new.fetch
